fbb4b6b148bee8e22b99c15a77a601727ef57ec7
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2017-04-06 18:16:42 +0800

    react-webpack-boiler

---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SampleComponent from './components/sample';
+
+const container = document.getElementById('sample-container');
+ReactDOM.render(<SampleComponent />, container);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var appPath = path.join(__dirname, 'app');
+var outputPath = path.join(__dirname, 'scripts');
+
+module.exports = [{
+  entry: {
+    index: appPath + '/index.jsx',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react'],
+        compact: false,
+        plugins: ['transform-runtime']
+      },    
+    }]     
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: outputPath,
+    filename: '[name].js'
+  },
+}]
